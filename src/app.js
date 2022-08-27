4a7a5cc4da0060f0a823353542855a205a15a801
Samuel Yoo <samueyoo@gmail.com>
2022-08-26 19:17:56 -0500

    Enabled CORS routing

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+//Added:
+const cors = require("cors");
 
 const corsEnabledRouter = require("./cors-enabled/cors-enabled.router");
 const corsNotEnabledRouter = require("./cors-not-enabled/cors-not-enabled.router");
@@ -7,6 +9,8 @@ const corsNotEnabledRouter = require("./cors-not-enabled/cors-not-enabled.router
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
 
+//Added:
+app.use(cors());
 app.use(express.json());
 
 app.use("/cors-enabled", corsEnabledRouter);
--- src/cors-enabled/cors-enabled.router.js
+++ src/cors-enabled/cors-enabled.router.js
@@ -1,17 +1,29 @@
 const router = require("express").Router();
 const controller = require("./cors-enabled.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+//Added:
+const cors = require("cors");
+//const corsDelete = cors({methods: "DELETE"});
+//router.use(cors());
 
 router
   .route("/:corsId")
+  //.all(cors())
   .get(controller.read)
   .put(controller.update)
+  //.delete(controller.delete)
+  //Added:
+  //.delete(cors(), controller.delete)
+  //.delete(corsDelete, controller.delete)
+  //.options(corsDelete)
   .delete(controller.delete)
   .all(methodNotAllowed);
 
 router
   .route("/")
-  .get(controller.list)
+  //Removed: .get(controller.list)
+  //Added:
+  .get(cors(), controller.list)
   .post(controller.create)
   .all(methodNotAllowed);
 
